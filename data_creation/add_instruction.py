d283c6c276a48680e48b0577b2c4cf45d77a3416
Kaiser <kaiserwholearns@gmail.com>
2025-03-13 12:10:58 -0400

    Fixed minor typo for instruction adding.

--- data_creation/add_instruction.py
+++ data_creation/add_instruction.py
@@ -64,7 +64,7 @@ def knowledge_free_tasks(raw_dataset):
             # Whether the summariztion can still be used to answer the question
             example[f"KF_{context_type}_openai_valid"] = is_valid(context=summary, question=example["question"], answer=example[f"{context_type}_answer"], checker="openai")
             example[f"KF_{context_type}_tog_valid"] = is_valid(context=summary, question=example["question"], answer=example[f"{context_type}_answer"], checker="tog")
-            if not example[f"KF_{context_type}_valid"]:
+            if not example[f"KF_{context_type}_tog_valid"] or not example[f"KF_{context_type}_openai_valid"]:
                 print("Detected one invalid instance after summarziation.")
             # example[f"{context_type}_KF_output"] = 
             # len(re.findall(r'[A-Za-z]', example[f"{context_type}_context"]))
@@ -81,27 +81,28 @@ def contextual_knowledge_tasks(raw_dataset):
     Create knowledge free tasks data
     """
     system_prompt = "You are a question-answering system that strictly answers questions based only on the given passage. Do not use external knowledge or make assumptions beyond what is explicitly stated. If the answer is not present in the passage, respond with 'The passage does not provide this information.'"
-    def create_kf_instance(example):
+    def create_ck_instance(example):
         for context_type in CONTEXT_TYPES:
             example[f"{context_type}_CK_input"] = system_prompt + "Question: " + example["question"] + "\nPassage: " + example[f"{context_type}_context"] + "\nAnswer: "
             example[f"{context_type}_CK_output"] =  example[f"{context_type}_answer"]
         return example
-    processed_dataset = raw_dataset.map(create_kf_instance)
+    processed_dataset = raw_dataset.map(create_ck_instance)
     # Write to local
     os.makedirs(os.path.join(os.path.join(os.environ["data_dir"], "task_data")), exist_ok=True)
     processed_dataset.to_json(os.path.join(os.environ["data_dir"], "task_data", f"{model_name}_contextual_knowledge.jsonl"))
     return processed_dataset
 
 def parametric_knowledge_tasks(raw_dataset):
-    # TODO: Create knowledge free tasks data
     # TODO: What is the best way for this task? Should we allow the model to combine? Should we pass both contexts?
-    system_prompt = "You are a knowledgeable question-answering system. You will be given a context and a question. Your task is to answer the question using your best possible knowledge while evaluating whether the provided context is reliable or accurate. Follow these rules: 1. If the context aligns with your knowledge, use it to support your answer. 2. If the context contradicts your knowledge, ignore the given context that appears unreliable and provide a corrected answer. 3. If the context provides additional details but does not contradict your knowledge, integrate both sources. 4. If you do not know the answer, state 'I do not have enough information to answer this confidently.'"
+    system_prompt = "You are a knowledgeable question-answering system. You will be given a context and a question. Your task is to answer the question using your best possible knowledge. You can combine your own knowledge along with the knowledge provided by the source. However, the provided source is not always reliable."
+    # v0-potential to try
+    # "Follow these rules: 1. If the context aligns with your knowledge, use it to support your answer. 2. If the context contradicts your knowledge, ignore the given context that appears unreliable and provide a corrected answer. 3. If the context provides additional details but does not contradict your knowledge, integrate both sources. 4. If you do not know the answer, state 'I do not have enough information to answer this confidently.'"
     def create_pk_instance(example):
         for context_type in CONTEXT_TYPES:
             example[f"{context_type}_PK_input"] = system_prompt + "Question: " + example["question"] + "\nPassage: " + example[f"{context_type}_context"] + "\nAnswer: "
             example[f"{context_type}_PK_output"] =  example[f"{context_type}_answer"]
         return example
-    processed_dataset = raw_dataset.map(create_kf_instance)
+    processed_dataset = raw_dataset.map(create_pk_instance)
     # Write to local
     os.makedirs(os.path.join(os.path.join(os.environ["data_dir"], "task_data")), exist_ok=True)
     processed_dataset.to_json(os.path.join(os.environ["data_dir"], "task_data", f"{model_name}_parametric_knowledge.jsonl"))
@@ -117,6 +118,6 @@ if __name__ == "__main__":
     # Load dataset
     raw_dataset = load_dataset("json", data_files=os.path.join(os.environ["data_dir"], "final_data_filtered", f"{model_name}_exampleLPC_v3.jsonl"))["train"]
 
-    knowledge_free_tasks(raw_dataset)
+    # knowledge_free_tasks(raw_dataset)
     # contextual_knowledge_tasks(raw_dataset)
-    # parametric_knowledge_tasks(raw_dataset)
+    parametric_knowledge_tasks(raw_dataset)
